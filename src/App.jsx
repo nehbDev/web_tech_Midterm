5463e64cde504cd51b6a85cfd236fe0c16136b2a
nehbDev <bhencyrisjohnsagaral@thelewiscollege.edu.ph>
2025-03-15 17:23:59 +0800

    Update App.jsx with corrected Navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
